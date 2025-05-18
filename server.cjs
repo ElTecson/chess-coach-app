28d112b0a17735bd85f4983f985eec5aa6ffef49
Emmanuel Leu Tecson <emmanuelleutecson@gmail.com>
2025-05-18 14:21:33 +0800

    feat: setup express js

---
+++ server.cjs
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express in root!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening at http://localhost:${PORT}`);
+});
+
