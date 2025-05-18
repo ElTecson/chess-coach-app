d8692ad5c36b39d65a80e21100b52173940bb499
Emmanuel Leu Tecson <emmanuelleutecson@gmail.com>
2025-05-18 16:01:45 +0800

    feat: connect to MongoDB Atlas on server run

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { connectDB } from './db.js';
 const app = express();
 const PORT = 5000;
 
@@ -8,7 +9,12 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express in root!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server listening at http://localhost:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server listening at http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.error('Failed to connect to DB:', err);
+  process.exit(1);
 });
 
