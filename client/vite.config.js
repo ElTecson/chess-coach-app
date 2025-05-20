b54160bff63fb08169d46a3fafe115c2c1b88cf2
Emmanuel Leu Tecson <emmanuelleutecson@gmail.com>
2025-05-20 19:24:50 +0800

    feat: make '@' an alias to client/src/ folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+// Define __dirname (for eslint)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +13,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
     },
   },
 })
