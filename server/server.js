b7c33dd6d98f7025bfe45823457a7e869325a176
Emmanuel Leu Tecson <emmanuelleutecson@gmail.com>
2025-05-20 14:26:00 +0800

    fix: remove useless express app route

--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const PORT = 5000;
 
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express in root!' });
-});
-
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server listening at http://localhost:${PORT}`);
