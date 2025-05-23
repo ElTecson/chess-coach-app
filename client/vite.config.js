d0cdcf1d1568357fa593c8a4b198e0088eb43e2f
Emmanuel Leu Tecson <emmanuelleutecson@gmail.com>
2025-05-23 17:56:56 +0800

    feat: install and setup vite-plugin-svgr for svg files

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url';
 
+// Setup vite-plugin-svgr for svg images file handling
+import svgr from 'vite-plugin-svgr'
+
 // Define __dirname (for eslint)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
